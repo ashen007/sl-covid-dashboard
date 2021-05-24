a432785e40d01cf17ce2c3d96310f40d32d65f9e
ashen007 <asheniranga@gmail.com>
2021-05-24 20:39:52 +0530

    web app created

--- dashboards/local/__init__.py
+++
@@ -1,9 +0,0 @@
-import dash
-
-
-def init_dashboard(server):
-    dash_app = dash.Dash(server=server,
-                         name='',
-                         url_base_pathname='/local/')
-
-    return dash_app
--- dashboards/overall/__init__.py
+++
@@ -1,9 +0,0 @@
-import dash
-
-
-def init_index_page(server):
-    dash_app = dash.Dash(server=server,
-                         name='',
-                         url_base_pathname='/')
-
-    return dash_app
